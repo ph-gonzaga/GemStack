c95b60bcd16112f8c3577beef7dc0f7a858e6ccb
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2020-01-11 10:55:10 -0300

    Create fundamentos ruby

---
+++ fundamentos/ruby/metodos.rb
@@ -0,0 +1,6 @@
+def soma(valor1, valor2)
+  valor1 + valor2
+end
+
+resultado = soma(15, 5)
+puts resultado
---
+++ fundamentos/ruby/vars.rb
@@ -0,0 +1,14 @@
+#Variaveis, metodos e campos sao criados no padrao snake_case (ruby)
+# O tipo da variavel e definido quando recebe o valor (Forte / Dinamico)
+
+nome_completo = "Antonio"
+idade = 36
+salario = 1000.00
+
+puts nome_completo
+puts idade
+puts salario
+
+puts nome_completo.class
+puts idade.class
+puts salario.class
